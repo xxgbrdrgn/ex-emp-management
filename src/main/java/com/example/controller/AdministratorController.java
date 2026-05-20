68afd4165a36b8e656bb63d4d85344732a6feb7a
xxgbrdrgn <xxgbrdrgn@outlook.jp>
2026-05-20 14:12:45 +0900

    デバッグしようとしたが、 結局変更せず

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -19,5 +22,13 @@ public class AdministratorController {
     @GetMapping("/to-insert")
     public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert";
+    }
+
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        Administrator administrator = new Administrator();
+        BeanUtils.copyProperties(form, administrator);
+        administratorService.insert(administrator);
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,13 @@ public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository;
 
+    /**
+     * 管理者情報を挿入する.
+     *
+     * @param administrator 挿入する管理者情報を持つadministratorオブジェクト
+     */
+    public void insert(Administrator administrator) {
+        administratorRepository.insert(administrator);
+    }
 
 }
